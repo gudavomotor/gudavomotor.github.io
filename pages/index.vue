58f52ea62773f26ccb1e96fbeb0083d608d297b1
gudavomotor <gudavo@foxmail.com>
2019-11-13 17:19:05 +0800

    更新首页

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,13 @@
 <template>
   <div class="container">
     <div>
-      <logo />
+      <img style="width: 400px;" src="/DSC_0082.jpg" />
       <h1 class="title">
-        gudavomotor.github.io
+        gudavomotor.com
       </h1>
       <h2 class="subtitle">
-        My striking Nuxt.js project
+        Site is coming soon ~~
       </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
     </div>
   </div>
 </template>
